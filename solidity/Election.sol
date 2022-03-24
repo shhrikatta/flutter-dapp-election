e4dd749cfedfec66ca51da4a9f72bc53519f2d7a
charan <shhrikatta@gmail.com>
2022-03-25 01:08:48 +0530

    added Elections solidity code file

---
+++ solidity/Election.sol
@@ -0,0 +1,63 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Election {
+    struct Candidate {
+        string name;
+        uint numVotes;
+    }
+
+    struct Voter {
+        string name;
+        bool isAuthorised;
+        uint whom;
+        bool isVoted;
+    }
+
+    address public owner;
+    string public electionName;
+
+    mapping(address=>Voter) public voters;
+    Candidate[] public candidates;
+
+    uint public totalVotes;
+
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    // start the election and init the admin and name of the election
+    function startElection(string memory _electionName) public {
+        owner = msg.sender;
+        electionName = _electionName;
+    }
+
+    // only owner can add a candidate
+    function addCandidate(string memory _candidateName) public ownerOnly {
+        candidates.push(
+            Candidate(
+                _candidateName,
+                0
+            )
+        );
+    }
+
+    function authoriseVoter(address _voterAddress) public ownerOnly {
+        voters[_voterAddress].isAuthorised = true;
+    }
+
+    function getNumCandidates() public view returns(uint) {
+        return candidates.length;
+    }
+
+    function vote(uint _candidateIndex) public {
+        require(!voters[msg.sender].isVoted);
+        require(voters[msg.sender].isAuthorised);
+        voters[msg.sender].whom = _candidateIndex;
+        voters[msg.sender].isVoted = true;
+        candidates[_candidateIndex].numVotes++;
+        totalVotes++;
+    }
+}
