f754a9d9211c57054fa878524b79fb795e760525
charan <shhrikatta@gmail.com>
2022-03-28 00:23:08 +0530

    added backend calling functionality

--- lib/services/functions.dart
+++ lib/services/functions.dart
@@ -1,1 +1,90 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dapp_election/utils/constant.dart';
+import 'package:web3dart/web3dart.dart';
 
+Future<DeployedContract> loadContract() async {
+  String abi = await rootBundle.loadString('assets/abi.json');
+  String contractAddress = contract_address;
+  final contract = DeployedContract(ContractAbi.fromJson(abi, contract_name),
+      EthereumAddress.fromHex(contractAddress));
+
+  return contract;
+}
+
+Future<String> callFunction(String functionName, List<dynamic> args,
+    Web3Client ethClient, String privateKey) async {
+  EthPrivateKey credentials = EthPrivateKey.fromHex(privateKey);
+  DeployedContract deployedContract = await loadContract();
+  final ethFunction = deployedContract.function(functionName);
+  final res = await ethClient.sendTransaction(
+      credentials,
+      Transaction.callContract(
+          contract: deployedContract, function: ethFunction, parameters: args));
+
+  return res;
+}
+
+Future<String> startElection(String name, Web3Client ethClient) async {
+  var response =
+      await callFunction(start_election, [name], ethClient, owner_private_key);
+
+  if (kDebugMode) {
+    print('Election started successfully');
+  }
+
+  return response;
+}
+
+Future<String> addCandidate(String name, Web3Client ethClient) async {
+  var response =
+      await callFunction(add_candidate, [name], ethClient, owner_private_key);
+
+  if (kDebugMode) {
+    print('Candidate added successfully');
+  }
+
+  return response;
+}
+
+Future<String> authVoter(String address, Web3Client ethClient) async {
+  var response = await callFunction(auth_voter,
+      [EthereumAddress.fromHex(address)], ethClient, owner_private_key);
+
+  if (kDebugMode) {
+    print('Candidate added successfully');
+  }
+
+  return response;
+}
+
+Future<List<dynamic>> ask(
+    String funcName, List<dynamic> args, Web3Client ethClient) async {
+  final contract = await loadContract();
+  final ethFunc = contract.function(funcName);
+  final res =
+      ethClient.call(contract: contract, function: ethFunc, params: args);
+
+  return res;
+}
+
+Future<List> voterCount(Web3Client ethClient) async {
+  List<dynamic> res = await ask(get_total_candidates, [], ethClient);
+
+  if (kDebugMode) {
+    print('Candidate added successfully');
+  }
+
+  return res;
+}
+
+Future<String> vote(int candidateIndex, Web3Client ethClient) async {
+  final response = await callFunction(vote_function,
+      [BigInt.from(candidateIndex)], ethClient, voter_private_key);
+
+  if (kDebugMode) {
+    print('vote counted successfully');
+  }
+
+  return response;
+}
--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -5,3 +5,11 @@ String owner_private_key =
     "638086b44e6bcacd2b4579a7b30b23daad73441b9d4f4d54cd588fd425bf831b";
 String voter_private_key =
     "8214300375174c3129f5149c0b1e08484ddc3ad3db49abda4f06a56976857902";
+
+String contract_address = "";
+String contract_name = "Election";
+String start_election = "startElection";
+String add_candidate = "addCandidate";
+String auth_voter = "authoriseVoter";
+String get_total_candidates = "getNumCandidates";
+String vote_function = "vote";
