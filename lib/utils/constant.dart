1c0159f79e9674efd5aba9db15bc2f41946164ae
m1404639 <m1404639@globalia.com>
2022-07-07 23:54:31 +0530

    removal of voters private key

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -2,11 +2,11 @@ String BASE_URL =
     "https://eth-kovan.alchemyapi.io/v2/UhtUS-1axixefFTv46bDe7Xw8rVzUOxo";
 
 String owner_private_key =
-    "638086b44e6bcacd2b4579a7b30b23daad73441b9d4f4d54cd588fd425bf831b";
+    "";
 String voter_private_key =
-    "8214300375174c3129f5149c0b1e08484ddc3ad3db49abda4f06a56976857902";
+    "";
 
-String contract_address = "0xEc06910E94F16Df6817D49D846d286053595A35c";
+String contract_address = "";
 String contract_name = "Election";
 String start_election = "startElection";
 String add_candidate = "addCandidate";
